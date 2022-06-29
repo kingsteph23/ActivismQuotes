29491424d793943a3335b3ad6aebdd97b5072293
Stephen <75145009+kingsteph23@users.noreply.github.com>
2022-06-29 12:45:28 -0400

    activismquotes.java

--- activismquotes.java
+++ activismquotes.java
@@ -1,51 +1,158 @@
-import java.util.*;
 
-public class main {
-    private String userchoice;
-// for countries
-    public static  String countries(String userchoice){
-            Scanner scan = new Scanner(System.in);
-        System.out.println("Select a region(Input either 1,2,3, or 4):"+
-        "\n(1)Americas"+
-        "\n(2)Europe"+
-        "\n(3)Africa"+
-        "\n(4)Asia");
+public class activismquotes {
+    
+    private String country;
 
 
-        
-        userchoice = scan.nextLine();
+public String asianactivists(){
 
-        if(userchoice.equals("1")){
-            System.out.println("Welcome to the  Americas!");
-        }else if(userchoice.equals("2")){
-            System.out.println("Welcome to Europe!");
-        }else if(userchoice.equals("3")){
-            System.out.println("Welcome to the African continent!");
-        }else if(userchoice.equals("4")){
-            System.out.println("Welcome to the continent of Asia!");
-        }
-            
-        
 
-        return userchoice;
-   }
+/*Al weiwei
+“The world is not changing if you don’t shoulder the burden of responsibility.”
+“Freedom is a pretty strange thing. Once you’ve experienced it, 
+it remains in your heart, and no one can take it away. Then, as an individual, 
+you can be more powerful than a whole country.”
+„A land that rejects the truth, barricades itself against change 
+and lacks the spirit of freedom is hopeless.“ Source: https://quotepark.com/authors/ai-weiwei/quotes-about-freedom/ Source: https://quotepark.com/authors/ai-weiwei/quotes-about-freedom/ Source: https://quotepark.com/authors/ai-weiwei/quotes-about-freedom/ Source: https://quotepark.com/authors/ai-weiwei/quotes-about-freedom/ Source: https://quotepark.com/authors/ai-weiwei/quotes-about-freedom/
 
-   //topic
-   public static  String topics(String usertopic){
+
+
+//Malala Yousafzai
+When the whole world is silent, even one voice becomes powerful.”
+“Don’t be afraid—if you are afraid you can’t move forward.”
+“Let us make our future now, and let us make our dreams tomorrow’s reality.”
+Ghandi
+
+*/
+return country;
+}
+
+public String americasactivists(String userchoice){
+
+    /*
+    mlk- “There comes a time when one must take a position that is neither safe, 
+    nor politic, nor popular, but he must take it because conscience tells him it is right.”
+
+    “Science investigates; religion interprets. Science gives man knowledge, 
+    which is power; religion gives man wisdom, 
+    which is control. Science deals mainly with facts; religion deals mainly with values. 
+    The two are not rivals.”
+
+
+    “Nothing in the world is more dangerous than sincere ignorance and conscientious stupidity.”
+
+
+    “Our lives begin to end the day we become silent about things that matter.”
+
+    Injustice anywhere is a threat to justice everywhere
+     "We must accept finite disappointment, but never lose infinite hope."
+
+
+    //james baldwin
+    “You think your pain and your 
+    heartbreak are unprecedented in the history of the world, but then you read.”
+       
+    “People can cry much easier than they can change.”
+
+"The paradox of education is precisely this — 
+that as one begins to become conscious one begins to examine the society 
+in which he is being educated."
+
+“It is certain, in any case, that ignorance, allied with power, is the most ferocious enemy justice can have.”
+    
+“I imagine one of the reasons people 
+cling to their hates so stubbornly is because they sense, 
+once hate is gone, they will be forced to deal with pain.”
+
+    
+    //obama“Change will not come if we wait for some other person 
+    or some other time. We are the ones we’ve been waiting for. 
+    We are the change that we seek.”
+
+    “When we don’t pay close attention to the decisions made by our leaders, 
+    when we fail to educate ourselves about the major issues of the day, 
+    when we choose not to make our voices and opinions heard, 
+    that’s when democracy breaks down. That’s when power is abused. 
+    That’s when the most extreme voices in our society fill the void that we leave. 
+    That’s when powerful interests and their lobbyists are most able to buy access and influence in the corridors of power
+     – because none of us are there to speak up and stop them.” – Barack Obama
+
+     "the arc if the universe may bend toward justice, but it doesn't bend on its own"
+
+     "dont boo vote"
+
+    “Progress will come in fits and starts. 
+    It’s not always a straight line. It’s not always smooth path.”
+    “Change is never easy, but always possible.”
+    //malcom x
+    “A man who stands for nothing will fall for anything.”
+    “You don’t have to be a man to fight for freedom.
+     All you have to do is to be an intelligent human being.” 
+     Change Is Only a Good Thing If You Change in a Good Way.
+
+
+     Education is an important element in the struggle for human rights. 
+     It is the means to help our children and thereby increase self-respect. 
+
+     It is the process of mis-education that inhibits the full potential of a nation.
+
+     “Without education, you’re not going anywhere in this world.”
+    //angela davis
+    “We have to talk about liberating minds as well as liberating society.”
+
+    “It’s important to remain optimistic regardless of the situation you are facing.”
+
+     “You have to believe that you can change society. 
+     You have to do it all of the time and not just once in a while.”
+
+     “Freeing our minds is just as important as freeing ourselves from our situations.”
+
+    “In a prejudiced world, it’s better to be anti-racist than non racist.”
+
+
+    //cesar chavez
+    “Preservation of one’s own culture does not require contempt or disrespect for other cultures.”
+
+    "Once social change begins, it cannot be reversed. 
+    You cannot un-educate the person who has learned to read. 
+    You cannot humiliate the person who feels pride. 
+    You cannot oppress the people who are not afraid anymore."
+
+
+    "Society is made up of groups, and as long as the smaller groups do not have 
+    the same rights and the same protection as others...
+    it is not going to work. Somehow the guys in power have to be reached by 
+    counterpower, or through a  change in the hearts and minds, or change will not come."
+
+“True wealth is not measured in money or status or power. 
+It is measured in the legacy we leave behind for those we love and those we inspire.” Cesar Chavez
+
+“Once social change begins, it cannot be reversed. We have seen the future, and the future is ours.” Cesar Chavez
+
+We cannot seek achievement for ourselves and forget about progress and prosperity for our community.
+ Our ambitions must be broad enough to include the aspirations and needs of others, 
+ for their sakes and for our own.” Cesar Chavez
     
 
-return usertopic;
+
+    */
+
+    return userchoice;
+    }
+
+
+
+       public String africanactivists (){
+
+
+        /*nelson mandela
+        //lemyah gbowee
+        desmond tutu
+
+*/
+            return country;
+            }
+
+
 }
 
-//
-
-
-
-   //main method 
- public static void main(String[] args) {
-    
-      countries("1");
-
-}
-//end of class
-}
