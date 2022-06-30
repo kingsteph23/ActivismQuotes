3fd1d00966f21faabcb9862f8a4eaa4d393d8d0e
kingsteph23 <75145009+kingsteph23@users.noreply.github.com>
2022-06-30 15:42:24 -0400

    Added quoters

--- main.java
+++ main.java
@@ -2,73 +2,72 @@ import java.util.*;
 
 public class main {
 
-
+//Asian activist
 public static void asianactivists(){
     ArrayList<String> asianactivists = new ArrayList<>();
   
 //ali weiwei
     asianactivists.add("The world is not changing if you don’t shoulder the burden of responsibility.");
-    asianactivists.add("\"The world is not changing if you don’t shoulder the burden of responsibility\"");
+    asianactivists.add("\"The world is not changing if you don’t shoulder the burden of responsibility\"-ali weiwei");
     asianactivists.add("\"Freedom is a pretty strange thing. Once you’ve experienced it, " +
                        "\nit remains in your heart, and no one can take it away. Then, as an individual," +
-                      "\nyou can be more powerful than a whole country.\"");
+                      "\nyou can be more powerful than a whole country.\"-ali weiwei");
     asianactivists.add("\"A land that rejects the truth, barricades itself against change "+
-                       "\nand lacks the spirit of freedom is hopeless.\"");
+                       "\nand lacks the spirit of freedom is hopeless.\"-ali weiwei");
 
 
 
 
 //Malala Yousafzai
-asianactivists.add("\"When the whole world is silent, even one voice becomes powerful.\"");
-asianactivists.add("\"Don’t be afraid—if you are afraid you can’t move forward.\"");
-asianactivists.add("\"Let us make our future now, and let us make our dreams tomorrow’s reality.\"");
+asianactivists.add("\"When the whole world is silent, even one voice becomes powerful.\"-Malala Yousafzai");
+asianactivists.add("\"Don’t be afraid—if you are afraid you can’t move forward.\"-Malala Yousafzai");
+asianactivists.add("\"Let us make our future now, and let us make our dreams tomorrow’s reality.\"-Malala Yousafzai");
 
-//Ghandi
 
 int index = (int)(Math.random() * asianactivists.size());
 System.out.println("Asia activist:"
                 +asianactivists.get(index));
 
-
+//Asian activist quotes
 }
-
+//americanactivists quotes
 public static void americasactivists(){
     ArrayList<String> americanactivists = new ArrayList<>();
      //mlk
      americanactivists.add("\"There comes a time when one must take a position that is neither safe,"+
-     "\nnor politic, nor popular, but he must take it because conscience tells him it is right\"");
+     "\nnor politic, nor popular, but he must take it because conscience tells him it is right\"-Martin Luther King Jr");
 
      americanactivists.add("\"Science investigates; religion interprets. Science gives man knowledge,"+
      "\nwhich is power; religion gives man wisdom,"+
      "\nwhich is control. Science deals mainly with facts; religion deals mainly with values. "+
-     "\nThe two are not rivals.\"");
+     "\nThe two are not rivals.\"-Martin Luther King Jr");
 
-     americanactivists.add("\"Nothing in the world is more dangerous than sincere ignorance and conscientious stupidity.\"");
+     americanactivists.add("\"Nothing in the world is more dangerous than sincere ignorance and conscientious stupidity.\"-Martin Luther King Jr");
 
-     americanactivists.add("\"Our lives begin to end the day we become silent about things that matter.\"");
+     americanactivists.add("\"Our lives begin to end the day we become silent about things that matter.\"-Martin Luther King Jr");
   
-     americanactivists.add("\"Injustice anywhere is a threat to justice everywhere\"");
+     americanactivists.add("\"Injustice anywhere is a threat to justice everywhere\"-Martin Luther King Jr");
      
 
-     americanactivists.add("\"We must accept finite disappointment, but never lose infinite hope.\"");
+     americanactivists.add("\"We must accept finite disappointment, but never lose infinite hope.\"-Martin Luther King Jr");
     
 
 
     //james baldwin
     americanactivists.add("\"You think your pain and your "+
-    "\nheartbreak are unprecedented in the history of the world, but then you read.\"");
+    "\nheartbreak are unprecedented in the history of the world, but then you read.\"-James Baldwin");
     
-    americanactivists.add("\"People can cry much easier than they can change.\"");
+    americanactivists.add("\"People can cry much easier than they can change.\"-James Baldwin");
 
     americanactivists.add("\"The paradox of education is precisely this — "+
     "\nthat as one begins to become conscious one begins to examine the society"+
-    "\nin which he is being educated.\"");
+    "\nin which he is being educated.\"-James Baldwin");
        
-    americanactivists.add("\"It is certain, in any case, that ignorance, allied with power, is the most ferocious enemy justice can have.\"");
+    americanactivists.add("\"It is certain, in any case, that ignorance, allied with power, is the most ferocious enemy justice can have.\"-James Baldwin");
     
     americanactivists.add("\"I imagine one of the reasons people"+
     "\ncling to their hates so stubbornly is because they sense," + 
-    "\nonce hate is gone, they will be forced to deal with pain.\"");
+    "\nonce hate is gone, they will be forced to deal with pain.\"-James Baldwin");
     
 
     
@@ -76,7 +75,7 @@ public static void americasactivists(){
 
     americanactivists.add("\"Change will not come if we wait for some other person"+
     "or some other time. We are the ones we’ve been waiting for."+
-    "We are the change that we seek.\"");
+    "We are the change that we seek.\"-Barack Obama");
     
     americanactivists.add("\"When we don’t pay close attention to the decisions made by our leaders,"+
     "when we fail to educate ourselves about the major issues of the day,"+
@@ -84,132 +83,135 @@ public static void americasactivists(){
     "that’s when democracy breaks down. That’s when power is abused."+
     "That’s when the most extreme voices in our society fill the void that we leave." +
     "That’s when powerful interests and their lobbyists are most able to buy access and influence in the corridors of power"+
-     "when– because none of us are there to speak up and stop them.\"");
+     "when– because none of us are there to speak up and stop them.\"-Barack Obama");
     
      americanactivists.add("\"the arc if the universe may bend toward justice, but it doesn't bend on its own\"");
 
-     americanactivists.add("\"dont boo vote\"");
+     americanactivists.add("\"dont boo vote\"-Barack Obama");
 
      americanactivists.add("\"Progress will come in fits and starts"+
-     "\nIts not always a straight line. Its not always smooth path.\"");
-     americanactivists.add("\"Change is never easy, but always possible.\"");
+     "\nIts not always a straight line. Its not always smooth path.\"-Barack Obama");
+     americanactivists.add("\"Change is never easy, but always possible.\"-Barack Obama");
 
      
 
     
     //malcom x
-    americanactivists.add("\"A man who stands for nothing will fall for anything.\"");
+    americanactivists.add("\"A man who stands for nothing will fall for anything.\"-Malcom X");
 
     americanactivists.add("\"You don’t have to be a man to fight for freedom." +
-    "\nAll you have to do is to be an intelligent human being.”\"");
-    americanactivists.add("\nChange Is Only a Good Thing If You Change in a Good Way\"");
+    "\nAll you have to do is to be an intelligent human being.”\"-Malcom X");
+    americanactivists.add("\nChange Is Only a Good Thing If You Change in a Good Way\"-Malcom X");
     
     americanactivists.add("\"Education is an important element in the struggle for human rights. "+
     "\nIt is the means to help our children and thereby increase self-respect."+
     "\nIt is the process of mis-education that inhibits the full potential of a nation." +
-    "\nWithout education, you’re not going anywhere in this world.”\"");
+    "\nWithout education, you’re not going anywhere in this world.”\"-Malcom X");
      
-    americanactivists.add("\"A man who stands for nothing will fall for anything.\"");
+    americanactivists.add("\"A man who stands for nothing will fall for anything.\"-Malcom X");
      
 
      
     //angela davis
-    americanactivists.add("\"We have to talk about liberating minds as well as liberating society.\"");
+    americanactivists.add("\"We have to talk about liberating minds as well as liberating society.\"-Angela Davis");
    
-    americanactivists.add("\"It’s important to remain optimistic regardless of the situation you are facing.\"");
+    americanactivists.add("\"It’s important to remain optimistic regardless of the situation you are facing.\"-Angela Davis");
 
     americanactivists.add("\"You have to believe that you can change society."+ 
-    "\nYou have to do it all of the time and not just once in a while.\"");
+    "\nYou have to do it all of the time and not just once in a while.\"-Angela Davis");
      
-    americanactivists.add("\"Freeing our minds is just as important as freeing ourselves from our situations.\"");
-    americanactivists.add("\"In a prejudiced world, it’s better to be anti-racist than non racist.\"");
+    americanactivists.add("\"Freeing our minds is just as important as freeing ourselves from our situations.\"-Angela Davis");
+    americanactivists.add("\"In a prejudiced world, it’s better to be anti-racist than non racist.\"-Angela Davis");
     
 
     //cesar chavez
-    americanactivists.add("\"Preservation of one’s own culture does not require contempt or disrespect for other cultures.\"");
+    americanactivists.add("\"Preservation of one’s own culture does not require contempt or disrespect for other cultures.\"-Cesar Chavez");
     
     americanactivists.add("\"Once social change begins, it cannot be reversed."+
     "\nYou cannot un-educate the person who has learned to read."+
     "\nYou cannot humiliate the person who feels pride."+
-    "\nYou cannot oppress the people who are not afraid anymore.\"");
+    "\nYou cannot oppress the people who are not afraid anymore.\"-Cesar Chavez");
    
     americanactivists.add("\"Society is made up of groups, and as long as the smaller groups do not have "+
     "\nthe same rights and the same protection as others..."+
     "\nit is not going to work. Somehow the guys in power have to be reached by "+
-    "\ncounterpower, or through a  change in the hearts and minds, or change will not come.\"");
+    "\ncounterpower, or through a  change in the hearts and minds, or change will not come.\"-Cesar Chavez");
 
     
     americanactivists.add("\"True wealth is not measured in money or status or power. "+
-    "\nIt is measured in the legacy we leave behind for those we love and those we inspire.\"");
+    "\nIt is measured in the legacy we leave behind for those we love and those we inspire.\"-Cesar Chavez");
 
-    americanactivists.add("\"Once social change begins, it cannot be reversed. We have seen the future, and the future is ours.\"");
+    americanactivists.add("\"Once social change begins, it cannot be reversed. We have seen the future, and the future is ours.\"-Cesar Chavez");
 
     americanactivists.add("\"We cannot seek achievement for ourselves and forget about progress and prosperity for our community."+
     "\nOur ambitions must be broad enough to include the aspirations and needs of others, "+
-    "\nfor their sakes and for our own\"");
+    "\nfor their sakes and for our own\"-Cesar Chavez");
     
     int windex = (int)(Math.random() * americanactivists.size());
 System.out.println("American activist:"
                 +americanactivists.get(windex));
 
-    
+    //end of American activist quotes
     }
 
 
+    //African activism quotes
 
        public static void africanactivists (){
         ArrayList<String> africanactivists = new ArrayList<>();
 
         //nelson mandela
     africanactivists.add("\"As long as poverty, injustice, and gross inequality persist in our world,"+
-                            "\nnone of us can truly rest.\"");
+                            "\nnone of us can truly rest.\"-Nelson Mandela");
 
-        africanactivists.add("\"Remember that hope is a powerful weapon even when all else is lost.\"");
+        africanactivists.add("\"Remember that hope is a powerful weapon even when all else is lost.\"-Nelson Mandela");
 
         africanactivists.add("\"Sometimes it falls upon a generation to be great."+
-        "\nYou can be that great generation. Let your greatness blossom\"");
+        "\nYou can be that great generation. Let your greatness blossom\"-Nelson Mandela");
 
         africanactivists.add("\"Of course the task will not be easy. But not to do this would be a crime against humanity, "+
-        "\nagainst which I ask all humanity now to rise up.\"");
+        "\nagainst which I ask all humanity now to rise up.\"-Nelson Mandela");
 
         africanactivists.add("\"It is not our diversity which divides us; it is not our ethnicity,"+ 
         "\bor religion or culture that divides us. "+
         "\nSince we have achieved our freedom, there can only be one division amongst us: "+
-        "\nbetween those who cherish democracy and those who do not\"");
+        "\nbetween those who cherish democracy and those who do not\"-Nelson Mandela");
 
     
         africanactivists.add("\"For to be free is not merely to cast off one’s chains,"+
-         "\nbut to live in a way that respects and enhances the freedom of others.\"");
+         "\nbut to live in a way that respects and enhances the freedom of others.\"-Nelson Mandela");
 
         //lemyah gbowee
         africanactivists.add("\" Don't wait for a Gandhi, don't wait for a King, don't wait for a Mandela. You are your own Mandela, "+
-        "\nyou are your own Gandhi, you are your own King.\"");
+        "\nyou are your own Gandhi, you are your own King.\"-lemyah gbowee");
 
-       africanactivists.add("\"The world is upside down, it's going to take a lot of hands to turn it right side up.\"");
+       africanactivists.add("\"The world is upside down, it's going to take a lot of hands to turn it right side up.\"-lemyah gbowee");
 
        africanactivists.add("\"Activism is something that no one can fake."+
        "You get angry. You cry. But you never throw in your towel, "+
-       "because that anger is what is propelling you to further action.\"");
+       "because that anger is what is propelling you to further action.\"-lemyah gbowee");
         //desmond tutu
         africanactivists.add("\"Do your little bit of good where you are" +
-        "\nit is those little bits of good put together that overwhelm the world\"");
+        "\nit is those little bits of good put together that overwhelm the world\"-desmond tutu");
 
         
-       africanactivists.add("\"The price of freedom is eternal vigilance\""); 
-       africanactivists.add("\"If you want peace, you don’t talk to your friends. You talk to your enemies\""); 
-       africanactivists.add("\"My father used to say, don’t raise your voice; improve your argument.\"");
-       africanactivists.add("\"Freedom and liberty lose out by default because good people are not vigilant.\"");
-       africanactivists.add("\"Exclusion is never the way forward on our shared paths to freedom and justice.\"");
+       africanactivists.add("\"The price of freedom is eternal vigilance\"-desmond tutu"); 
+       africanactivists.add("\"If you want peace, you don’t talk to your friends. You talk to your enemies\"-desmond tutu"); 
+       africanactivists.add("\"My father used to say, don’t raise your voice; improve your argument.\"-desmond tutu");
+       africanactivists.add("\"Freedom and liberty lose out by default because good people are not vigilant.\"-desmond tutu");
+       africanactivists.add("\"Exclusion is never the way forward on our shared paths to freedom and justice.\"-desmond tutu");
 
        int mindex = (int)(Math.random() * africanactivists.size());
        System.out.println("African activist:"
                        +africanactivists.get(mindex));
-        
+        //end of African method
             }
-
-//
 // for countries
 public static void countries(){
+
+    System.out.println("------------------------------------------------"+
+                        "\n     Activism Nation                          "+
+                        "\n------------------------------------------------");
     String userchoice;
     Scanner scan = new Scanner(System.in);
 System.out.println("Select a region(Input either 1,2,or 3):"+
@@ -232,7 +234,7 @@ if(userchoice.equals("1")){
     asianactivists();
 }
     
-//e nd of method
+//end of method
 }
 
 
